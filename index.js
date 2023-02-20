2c1b9a7a03ed74d6b296d30d2dfc429951cff1b8
unknown <tkiet1705@gmail.com>
2023-02-20 11:30:22 +0700

    add file index

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
-// const express = require("express");
-// const app = express();
-// const bodyParser = require("body-parser");
-// var cors = require("cors");
-// require("dotenv").config();
-// const port = 8000;
-
-// //--------------------------------------------------------------------------------------
-
-// const database = require("./config/database");
-// const route = require("./routes");
-
-// //--------------------------------------------------------------------------------------
-
-// database.connect();
-
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
-// route(app);
-
-// //--------------------------------------------------------------------------------------
-
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`);
-// });
-
 const express = require("express");
 const app = express();
-const port = 3000;
+const bodyParser = require("body-parser");
+var cors = require("cors");
+require("dotenv").config();
+const port = 8000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+//--------------------------------------------------------------------------------------
+
+const database = require("./src/config/database");
+const route = require("./src/routes");
+
+//--------------------------------------------------------------------------------------
+
+database.connect();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+route(app);
+
+//--------------------------------------------------------------------------------------
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
