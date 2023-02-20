bb5ab9b0f7479fc98bf1a94046ccd197873bc60b
unknown <tkiet1705@gmail.com>
2023-02-20 11:24:21 +0700

    firt commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
+// const express = require("express");
+// const app = express();
+// const bodyParser = require("body-parser");
+// var cors = require("cors");
+// require("dotenv").config();
+// const port = 8000;
+
+// //--------------------------------------------------------------------------------------
+
+// const database = require("./config/database");
+// const route = require("./routes");
+
+// //--------------------------------------------------------------------------------------
+
+// database.connect();
+
+// app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// route(app);
+
+// //--------------------------------------------------------------------------------------
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+// });
+
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-var cors = require("cors");
-require("dotenv").config();
-const port = 8000;
+const port = 3000;
 
-//--------------------------------------------------------------------------------------
-
-const database = require("./config/database");
-const route = require("./routes");
-
-//--------------------------------------------------------------------------------------
-
-database.connect();
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-route(app);
-
-//--------------------------------------------------------------------------------------
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
